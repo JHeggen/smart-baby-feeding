fc5d8a92001d987fba6993304abacfff52e40f07
Jonathan <jonathan@Jonathans-MacBook-Pro.local>
2017-05-01 19:18:15 -0700

    initial commit

---
+++ app/src/main/java/comp4900/bcit/ca/smart_baby_feeding/MainActivity.java
@@ -0,0 +1,13 @@
+package comp4900.bcit.ca.smart_baby_feeding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
