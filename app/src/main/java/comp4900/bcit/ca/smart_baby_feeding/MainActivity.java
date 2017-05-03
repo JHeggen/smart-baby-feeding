c03ac14de0ebaa5fc9b0c1730dcc7b057f14335c
pkao <paul_kao@hotmail.ca>
2017-05-03 10:46:30 -0700

    Signed-off-by: pkao <paul_kao@hotmail.ca>

--- app/src/main/java/comp4900/bcit/ca/smart_baby_feeding/MainActivity.java
+++ app/src/main/java/comp4900/bcit/ca/smart_baby_feeding/MainActivity.java
@@ -1,15 +1,22 @@
 package comp4900.bcit.ca.smart_baby_feeding;
 
+import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.greenrobot.greendao.query.Query;
+
 public class MainActivity extends AppCompatActivity {
-    
+
+    private RecipeDao recipeDao;
+    private Query<Recipe> recipeQuery;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        DaoSession daoSession = ((App)getApplication()).getDaoSession();
+        recipeDao = daoSession.getRecipeDao();
     }
 }
--- app/src/main/java/comp4900/bcit/ca/smart_baby_feeding/Recipe.java
+++ app/src/main/java/comp4900/bcit/ca/smart_baby_feeding/Recipe.java
@@ -2,6 +2,7 @@ package comp4900.bcit.ca.smart_baby_feeding;
 
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * Created by Pkao on 2017-05-02.
@@ -12,4 +13,30 @@ public class Recipe {
     private long id;
 
     private String name;
+
+    @Generated(hash = 1709081986)
+    public Recipe(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Generated(hash = 829032493)
+    public Recipe() {
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
