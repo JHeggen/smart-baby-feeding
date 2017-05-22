adae765516c337f264a60552d026e7404617a63c
pkao <paul_kao@hotmail.ca>
2017-05-22 12:13:02 -0700

    minor changes to gradle and dialog fragment

---
+++ app/src/main/java/a00907981/comp3717/bcit/ca/tabtest/RecipeList/ingredient_amount.java
@@ -0,0 +1,10 @@
+package a00907981.comp3717.bcit.ca.tabtest.RecipeList;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by Pkao on 2017-05-22.
+ */
+
+public class ingredient_amount extends DialogFragment {
+}
